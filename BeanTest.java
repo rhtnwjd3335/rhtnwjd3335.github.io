becf582c8981008bcb372b9aa116fe928eeec9e2
rhtnwjd3335 <144886180+rhtnwjd3335@users.noreply.github.com>
2023-10-09 02:11:35 +0900

    Create BeanTest.java

---
+++ BeanTest.java
@@ -0,0 +1,14 @@
+package bean.user;
+
+public class BeanTest {
+    String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+    
+}
