444e75c13f3eba10a1f7fc91ae87bdeb78ae5dec
rhtnwjd3335 <144886180+rhtnwjd3335@users.noreply.github.com>
2023-10-09 02:08:15 +0900

    Create Login.java

---
+++ Login.java
@@ -0,0 +1,12 @@
+package bean.user;
+
+public class Login {
+   private String userName;
+
+   public void setUserName(String userName) {
+		this.userName = userName;
+   }	
+	public String getUserName() {
+	return userName;
+}
+}
