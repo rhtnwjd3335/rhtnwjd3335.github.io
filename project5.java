0318d0603fa0ef40f848e5849587790294f97d26
rhtnwjd3335 <144886180+rhtnwjd3335@users.noreply.github.com>
2023-10-09 02:14:25 +0900

    Create project5.java

---
+++ project5.java
@@ -0,0 +1,14 @@
+package bean.user;
+
+public class project5 {
+	public static void main(String[] args) {
+		private int x1, x2;
+		private int y1, y2;
+		private int d;
+		
+		public int getd() {
+		    d=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		    }
+		    return;
+	}
+}
