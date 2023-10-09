b53abf2aa887a1623fd7fc28cd562086ea6751b8
rhtnwjd3335 <144886180+rhtnwjd3335@users.noreply.github.com>
2023-10-09 19:19:06 +0900

    Update project4.java

--- project4.java
+++ project4.java
@@ -1,18 +1,44 @@
 package bean.user;
 
 public class project4 {
-	public static void main(String[] args) {
-		private int num;
-		private int res;
-		
-		for(num; num<100; num++) {
-			int num2=0;
-			for(res=1; res<=num; num++) {
-				if(num%res == 0)
-					num2 = num2+1;
-			}
-			if (num2==2)
-				System.out.println(num+",");
-		}
-	    }
+	private int result=0;
+	private int i;
+	private int n;
+	private int j;
+	
+	public void setI(int i) {
+		this.i = i;
+	}
+	public int getI() {
+		return i;
+	}
+	public void setN(int n) {
+		this.n = n;
+	}
+	public int getN() {
+		return n;
+	}
+	public void setJ(int j) {
+		this.j = j;
+	}
+	public int getJ() {
+		return j;
+	}
+	public void setResult(int result) {
+		this.result = result;
+	}
+	public int getResult() {
+		for(int i=2; i<=n; i++) {
+            boolean flag = true;
+            for(int j=2; j<Math.sqrt(i); j++) { //두 번째 방법에서는 j<i 부분을 j<Math.sqrt(i) 로 바꾼다.
+                if(i%j==0) {
+                    flag = false;
+                    break;
+                }
+            }
+            
+            if(flag==true) result++;
+        }
+		return result;
+	}
 }
