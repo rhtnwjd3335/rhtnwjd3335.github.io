1464d1c665105d74e06c798a1f0b83cf8ce9e08e
rhtnwjd3335 <144886180+rhtnwjd3335@users.noreply.github.com>
2023-10-09 17:58:02 +0900

    Update project3.java

--- project3.java
+++ project3.java
@@ -1,13 +1,36 @@
 package bean.user;
 
 public class project3 {
-	private int num1, num2;
+	private int num1;
+	private int num2;
+	private int i=num1;
 	private int sum=0;
 	
-	public int getNum() {
-	    while(num1<=num2<=100) {
-	    	sum += num2++;
+	public void setNum1(int num1) {
+		this.num1 = num1;
+	}
+	public int getNum1() {
+		return num1;
+	}
+	public void setNum2(int num2) {
+		this.num2 = num2;
+	}
+	public int getNum2() {
+		return num2;
+	}
+	public void setI(int i) {
+		this.i = i;
+	}
+	public int getI() {
+		return i;
+	}
+	public void setSum(int sum) {
+		this.sum = sum;
+	}
+	public int getSum() {
+		while(i <= num2) {
+	    	sum += i++;
 	    }
-	    return this.num1;
-	    }
+		return sum;
+	}
 }
