3b641795fa62329ba07bcc9683766827b909764b
rhtnwjd3335 <144886180+rhtnwjd3335@users.noreply.github.com>
2023-10-09 20:56:12 +0900

    Update project5.java

--- project5.java
+++ project5.java
@@ -1,14 +1,41 @@
 package bean.user;
 
 public class project5 {
-	public static void main(String[] args) {
-		private int x1, x2;
-		private int y1, y2;
+		private int x1;
+		private int x2;
+		private int y1;
+		private int y2;
 		private int d;
 		
-		public int getd() {
-		    d=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-		    }
-		    return;
-	}
+		public void setX1(int x1) {
+			this.x1 = x1;
+		}
+		public int getX1() {
+			return x1;
+		}
+		public void setX2(int x2) {
+			this.x2 = x2;
+		}
+		public int getX2() {
+			return x2;
+		}
+		public void setY1(int y1) {
+			this.y1 = y1;
+		}
+		public int getY1() {
+			return y1;
+		}
+		public void setY2(int y2) {
+			this.y2 = y2;
+		}
+		public int getY2() {
+			return y2;
+		}
+		public void setD(int d) {
+			this.d = d;
+		}
+		public int getD() {
+			d = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+			return d;
+		}	
 }
