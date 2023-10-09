d97d295f4b4e980a15f0f9e4bd9ebad6b3d61575
rhtnwjd3335 <144886180+rhtnwjd3335@users.noreply.github.com>
2023-10-09 17:08:18 +0900

    Update project1.java

--- project1.java
+++ project1.java
@@ -1,7 +1,10 @@
 package bean.user;
 
 public class project1 {
-    private int num1, num2;
+    private int num1;
+    private int num2;
+    private int max;
+    private int min;
     private int startNum;
     private int endNum;
     
@@ -17,8 +20,32 @@ public class project1 {
     public int getEndNum() {
 		return endNum;
 	}
-	public int getNum() {
-		num1, num2 = (int)(Math.random()*(endNum-startNum)) + startNum;
-		return startNum;
+	public void setNum1(int num1) {
+		this.num1 = num1;
+	}
+	public int getNum1() {
+		num1 = (int)(Math.random()*(endNum-startNum)) + startNum;
+		return num1;
+	}
+	public void setNum2(int num2) {
+		this.num2 = num2;
+	}
+	public int getNum2() {
+		num2 = (int)(Math.random()*(endNum-startNum)) + startNum;
+		return num2;
+	}
+	public void setMax(int max) {
+		this.max = max;
+	}
+	public int getMax() {
+		max=endNum;
+		return max;
+	}
+	public void setMin(int min) {
+		this.min = min;
+	}
+	public int getMin() {
+		min=startNum;
+		return min;
 	}
 }
