b752024b402e7036a3ba5e6dce5e2c8eeabcecc3
rhtnwjd3335 <144886180+rhtnwjd3335@users.noreply.github.com>
2023-10-09 17:46:23 +0900

    Update project2.java

--- project2.java
+++ project2.java
@@ -1,13 +1,29 @@
 package bean.user;
 
 public class project2 {
-	private int num=1;
+	private int num;
+	private int i=1;
 	private int sum=0;
 	
+	public void setNum(int num) {
+		this.num = num;
+	}
 	public int getNum() {
-	    while(num<=100) {
-	    	sum += num++;
-	    }
 	    return this.num;
 	    }
+	public void setI(int i) {
+		this.i = i;
+	}
+	public int getI() {
+		return i;
+	}
+	public void setSum(int sum) {
+		this.sum = sum;
+	}
+	public int getSum() {		
+		while(i <= num) {
+			sum += i++;
+		}
+		return sum;
+	}
 }
