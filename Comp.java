4179f8ed6b39f84de02d37d858474e320bdf2c7e
rhtnwjd3335 <144886180+rhtnwjd3335@users.noreply.github.com>
2023-10-11 16:01:57 +0900

    Create Comp.java

---
+++ Comp.java
@@ -0,0 +1,14 @@
+package studpkg;
+
+public class Comp {
+	public Comp() {
+		System.out.println("Instance Generated!!");
+	}
+private int num;
+public void setNum(int num) {
+	this.num = num + 100;
+}
+public int getNum() {
+	return num;
+}
+}
